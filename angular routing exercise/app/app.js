1f90b74da2220604b9272529c82468a72ea07601
William-Bachman <william_bachman@outlook.com>
2015-04-22 17:57:17 -0400

    Angular routing example with Eric and Jason

---
+++ angular routing exercise/app/app.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module('app',['ngRoute']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'app/views/View1.html'
+    }).when('/View2', {
+        templateUrl: ''
+    }).otherwise({
+        redirectTo: '/'
+    })
+});
